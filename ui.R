2e445b8e65dc9422923d102b5e5eba6011cb5c4f
Cro-Magnon <robertoruizicochea@gmail.com>
2020-04-12 17:06:19 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,9 +1,19 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+secuencias <- list.files()
+secuencias <- secuencias[-c(2,5,6,7,8,9)]
 
 library(shiny)
+library(DT)
 
-
-secuencias <- list.files("~/Documents/corona/adn/corona/otros")
-
+# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   
   # Application title
@@ -16,7 +26,7 @@ shinyUI(fluidPage(
       #selectInput("seq1", "Secuencia1", choices = unique(secuencias), selected = NULL, multiple = FALSE,
        #           selectize = TRUE, width = NULL, size = NULL),
       
-      selectInput("seq2", "Secuencia2", choices = unique(secuencias), selected = NULL, multiple = FALSE,
+      selectInput("seq2", "Escoja la Secuencia a comparar:", choices = unique(secuencias), selected = NULL, multiple = FALSE,
                   selectize = TRUE, width = NULL, size = NULL),
       
       
@@ -24,8 +34,13 @@ shinyUI(fluidPage(
                    "Number of bins:",
                    min = 1,
                    max = 200,
-                   value = 30)
-    
+                   value = 30),
+      
+      h5("Herramienta para comparar el ARN del COVID19 peruano con el de otros países."),
+      h5("Data obtenida de GISAID.org"),
+      tags$a("Usted puede encontrar el código abierto AQUI", href="https://github.com/robertoruiz911/PERU_ARN_COVID"),
+      h5("Contacto: Roberto Ruiz Icochea"),
+      h5("mail: robertoruizicochea@gmail.com")
     ),
     
     
@@ -40,9 +55,13 @@ shinyUI(fluidPage(
                  
                  radioButtons("desconocidos", h4("Quitar enes"), choices = c("si", "no"), selected = "no"),
                  
+                 verbatimTextOutput("secuencia2_nombre"),
+                 
                  dataTableOutput("acidos_diferentes")
-                
-          
+                 
+      
+       
+       
     )
   )
 ))
